d1b896057bf6f8d2e13015af2ff95d1c97c18c3c
Giada15 <tascheriogiada@gmail.com>
2023-07-17 10:43:34 -0400

    Implement date counter feature using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = React.useState(0)
+  const [step, setStep] = React.useState(1)
+
+  const today = new Date()
+  const options = {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }
+
+  function formatDate(d) {
+    return new Intl.DateTimeFormat("en-US", options).format(d).replace(",", " ")
+  }
+
+  function handlePlusStep() {
+    setStep((prevStep) => prevStep + 1)
+  }
+
+  function handleMinusStep() {
+    setStep((prevStep) => prevStep - 1)
+  }
+
+  function handlePlusCount() {
+    setCount((prevCount) => prevCount + step)
+  }
+
+  function handleMinusCount() {
+    setCount((prevCount) => prevCount - step)
+  }
+
+  function getNewDate(date, days) {
+    const dateCopy = new Date(date)
+    dateCopy.setDate(date.getDate() + days)
+    return formatDate(dateCopy)
+  }
+
+  function displayNewDate(count, today) {
+    if (count >= 1)
+      return `${count} days from today is ${getNewDate(today, count)}`
+    else return `${Math.abs(count)} days ago was ${getNewDate(today, count)}`
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleMinusStep}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handlePlusStep}>+</button>
+      </div>
+      <div>
+        <button onClick={handleMinusCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handlePlusCount}>+</button>
+      </div>
+      <div>
+        {count === 0 ? (
+          <p>Today is {formatDate(today)}</p>
+        ) : (
+          <p>{displayNewDate(count, today)}</p>
+        )}
+      </div>
+    </div>
+  )
+}
